9d2f034ed9e7eb2d024ea5b12f4f52235f7cea5f
Fabián A. Saso <fsaso13@gmail.com>
2016-08-14 17:16:25 -0400

    Seeds con las intolerancias y los key components V1.0

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ Intolerance.create(name: "lactosa", minor_symptom: "", medium_symptom: "Náuseas
 	Defecación explosiva
 	Vómitos
 	Enrojecimiento periana", major_symptom: "", key_components: ["lactosa","lactose","leche","milk"])
+
 =begin
 # Intolerance.create(name: "gluten", minor_symptom: "", medium_symptom: "Diarrea crónica
 	Hinchazón abdominal
@@ -24,18 +25,18 @@ Intolerance.create(name: "lactosa", minor_symptom: "", medium_symptom: "Náuseas
 	Dolor de cabeza
 	Alteraciones del esmalte dental
 	Raquitismo
-	Fracturas espontáneas", major_symptom: "", key_components: [])
+	Fracturas espontáneas", major_symptom: "", key_components: ["trigo", "centeno", "cebada", "escanda", "espelta", "triticale", "farro", "kamut", "espelta verde", "bulgur y avena"])
 
 # Intolerance.create(name: "mani", minor_symptom: "", medium_symptom: "Dolor estomacal
 	Goteo nasal
 	Picazón de ojos
-	Ronchas (Urticaria) u hormigueo en los labios o la lengua", major_symptom: "", key_components: [])
+	Ronchas (Urticaria) u hormigueo en los labios o la lengua", major_symptom: "", key_components: ["mani","cacahuate","peanut,"frutos secos"])
 
 # Intolerance.create(name: "nueces", minor_symptom: "", medium_symptom: "Picazón o hinchazón de la boca, la lengua o los labios
 	Reacciones en la piel (Eccema, urticaria, hinchazón y enrojecimiento de la cara o las extremidades)
 	Los síntomas gastrointestinales (Dolor abdominal, náuseas, vómitos, diarrea)
 	Los síntomas respiratorios (Secreción o congestión nasal, tos, estornudos, respiración sibilante, dificultad para respirar)
-	Los síntomas cardiovasculares (Caída en la presión arterial, mareos, desmayos)", major_symptom: "", key_components: [])
+	Los síntomas cardiovasculares (Caída en la presión arterial, mareos, desmayos)", major_symptom: "", key_components: ["nuez","nut","nueces","frutos secos"])
 =end
 
 Intolerance.create(name: "apio", minor_symptom: "", medium_symptom: "Vómitos
@@ -50,18 +51,18 @@ Intolerance.create(name: "apio", minor_symptom: "", medium_symptom: "Vómitos
 	Eccema
 	Asma bronquial
 	Dolor gástrico
-	Shock anafiláctico", major_symptom: "", key_components: [])
+	Shock anafiláctico", major_symptom: "", key_components: ["mostaza","mustard"])
 
 # Intolerance.create(name: "huevo", minor_symptom: "", medium_symptom: "Reacciones cutáneas
     Síntomas gastrointestinales agudos y respiratorios
-    Reacción generalizada de anafilaxia (Junto con hipotensión, colapso vascular y disrritmias cardiacas)", major_symptom: "", key_components: [])
+    Reacción generalizada de anafilaxia (Junto con hipotensión, colapso vascular y disrritmias cardiacas)", major_symptom: "", key_components: ["egg","huevo", "Lecitina", "lisozima", "albúmina", "coagulante", "emulsificante", "gloubina", "livetina", "ovoalbúmina", "ovomucina", "ovomucoide", "otransferrina", "ovovitelina", "vitelina", "E-16b,luteína", "pigmento amarillo"])
 
 # Intolerance.create(name: "sesamo", minor_symptom: "", medium_symptom: "Dermatitis de contacto 
     Urticaria
     Rinitis alérgica
     Asma
     Dermatitis atópica
-    Anafilaxia", major_symptom: "", key_components: [])
+    Anafilaxia", major_symptom: "", key_components: ["sésamo","ajonjolí","sesame"])
 
 # Intolerance.create(name: "pescado", minor_symptom: "", medium_symptom: "Hinchazón de los labios
 	Prurito o picor en la cavidad bucal
@@ -72,18 +73,18 @@ Intolerance.create(name: "apio", minor_symptom: "", medium_symptom: "Vómitos
 	Diarrea
 	Ronchas graves
 	Dificultad respiratoria y asma
-	Náuseas y vómitos", major_symptom: "", key_components: [])
+	Náuseas y vómitos", major_symptom: "", key_components: ["fish","pescado","[ESPECIES DE PESCADO???]"])
 
 # Intolerance.create(name: "crustaceos", minor_symptom: "", medium_symptom: "Urticaria aguda
 	Asma
 	Rinitis
 	Anafilaxia
-	Síndrome de alergia oral", major_symptom: "", key_components: [])
+	Síndrome de alergia oral", major_symptom: "", key_components: ["crustáceos","crustacean","[ESPECIES DE CRUSTACEOS???]"])
 
-# Intolerance.create(name: "mariscos", minor_symptom: "", medium_symptom: "Urticaria aguda
+# Intolerance.create(name: "moluscos", minor_symptom: "", medium_symptom: "Urticaria aguda			
 	Asma
 	Anafilaxia
-	Síndrome de alergia oral", major_symptom: "", key_components: [])
+	Síndrome de alergia oral", major_symptom: "", key_components: ["moluscos","molluscs","[ESPECIES DE MOLUSCOS????]"])
 
 # Intolerance.create(name: "soya", minor_symptom: "", medium_symptom: "Hinchazón en labios, cara, lengua, garganta y otras partes del cuerpo
     Hormigueo en la boca
@@ -96,13 +97,13 @@ Intolerance.create(name: "apio", minor_symptom: "", medium_symptom: "Vómitos
     Pulso rápido o taquicardia
     Pérdida del conocimiento
     Aturdimiento
-    Anafilaxia: constricción de las vías respiratorias, incluyendo la inflamación de garganta", major_symptom: "", key_components: [])
+    Anafilaxia: constricción de las vías respiratorias, incluyendo la inflamación de garganta", major_symptom: "", key_components: [soy", "Mono-diglicerido", "Soya", "Soja", "Yuba", "TSF" ,"harina de soya texturizada", "TSP" ,"proteína de soya texturizada","TVP", "proteína vegetal texturizada", "Lecitina", "MSG", "lutamato monosodico"])
 
 # Intolerance.create(name: "sulfitos", minor_symptom: "", medium_symptom: "Estornudos
 	Secreción nasal (rinorrea)
 	Prurito (picor)
 	Erupción (aparecen granos, como urticaria)
-	Dolor abdominal o posiblemente asma", major_symptom: "", key_components: [])
+	Dolor abdominal o posiblemente asma", major_symptom: "", key_components: ["sulfitos","sulfites","Dióxido de azufre","E220","Sulfito sódico","E221","Sulfito ácido de sodio","E222","Metabisulfito sódico","Disulfito sódico","E223", "Metabisulfito potásico","Disulfito potásico","E224",  "Sulfito cálcico", "E226","Sulfito ácido de calcio","Bisulfito cálcico", "E227", "Sulfito ácido de potasio" ,"Bisulfito potásico" ,"E228"])
 =end
 Intolerance.create(name: "legumbres", minor_symptom: "", medium_symptom: "Síndrome de alergia oral
 	Urticaria

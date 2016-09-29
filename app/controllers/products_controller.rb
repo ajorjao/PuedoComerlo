2fc1e6922ac75a337b2e1772e24d2d1afaa8950a
Fabian Saso <Fabian Saso>
2016-09-29 16:41:03 -0400

    whoops ahora si está arreglado el destroy denounced product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -376,7 +376,7 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to 'denounced_products/', notice: 'Product was successfully destroyed.' }
+      format.html { redirect_to '/denounced_products', notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
